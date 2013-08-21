1bc90d6f1e2aa63dbf5b34e368112d711dffafa1
Joshua Bell <pensivearchitect@gmail.com>
2013-08-21 13:31:46 -0700

    Updated spec to do something useful

--- spec/hs_spec.rb
+++ spec/hs_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 describe Highscores do
-		pending "write it"
+		describe "#score" do
+				it "returns 0 for all lookup" do
+						lookup = Highscore.new
+						lambda { 1.time { lookup.total("Jebrim") } }
+						lookup.total.should == 2150
+				end
+		end
 end
