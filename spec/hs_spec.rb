ca74711fa9b824576d8f679884dd24fa06925a5e
Sam Broughton <sam@26th-zerk.co.uk>
2013-08-22 01:51:04 +0100

    Test refactoring and use of Shoulda gem

--- spec/hs_spec.rb
+++ spec/hs_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Osrs::Player do
 		describe "name validation" do
       it "error on invalid characters" do
-        
+        lambda { Osrs::Player.new "@£$%^&" }.should raise_error
       end
 		end
 end
