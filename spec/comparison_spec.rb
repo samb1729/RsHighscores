9e47cc7f221d6d9a4b2d7ba78c6d5f0aa664fc8a
Sam Broughton <sam@26th-zerk.co.uk>
2013-08-29 05:38:26 +0100

    Added EOF newlines

--- spec/comparison_spec.rb
+++ spec/comparison_spec.rb
@@ -2,5 +2,9 @@ require 'spec_helper'
 
 describe Osrs::Comparison do
   jebrim = Osrs::Player.new "jebrim", :force
-  foot   = Osrs::Player.new "foot",   :force 
+  foot   = Osrs::Player.new "foot",   :force
+
+  context "by name" do
+    # TODO: Write something here...
+  end
 end
