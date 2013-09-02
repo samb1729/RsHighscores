887739f80e1eeba4073cd61e9d705265062843b2
Sam Broughton <sam@26th-zerk.co.uk>
2013-09-02 15:33:54 +0100

    Wrote passing OSRS intergration

--- lib/rshighscores/oldschool.rb
+++ lib/rshighscores/oldschool.rb
@@ -1,11 +1,58 @@
 module RsHighscores
   module OldSchool
-    class Player
-      
+    class Player < RsHighscores::Player
+      def fetch_highscores
+        f = open("http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + @name, "User-Agent" => "Ruby/RsHighscoresGrabber")
+        
+        @raw_stats = f.readlines.map &:chomp # readlines preserves newlines??
+        @stats = RsHighscores::OldSchool::Stats.new @raw_stats
+      end
+    end
+
+    class Stats < RsHighscores::Stats
+      Skills = %w(Overall Attack Defence Strength
+                  Hitpoints Ranged Prayer Magic
+                  Cooking Woodcutting Fletching Fishing
+                  Firemaking Crafting Smithing Mining
+                  Herblore Agility Thieving Slayer
+                  Farming Runecrafting Hunter Construction)
+
+      ExpectedStatCount = 39
+      ActualStatCount = 23
+
+      def validate_raw_stats
+        raise "incorrect input length" unless @raw_stats.length == ExpectedStatCount
+      end
+
+      def parse_stats
+      validate_raw_stats
+
+      @stats = []
+      @raw_stats.take(ActualStatCount).each do |line|
+        raise "malformed raw stats" unless line =~ /\d+,\d+,\d+/
+        stat = line.split(",").map(&:to_i)
+
+        class << stat
+          def method_missing name, *args
+            case name
+            when :rank
+              return self[0]
+            when :level
+              return self[1]
+            when :xp
+              return self[2]
+            end
+          end
+        end
+
+        @stats << stat
+      end
+    end
     end
   end
 end
 
 module OSRS
   Player = RsHighscores::OldSchool::Player
+  Stats = RsHighscores::OldSchool::Stats
 end
--- spec/oldschool_spec.rb
+++ spec/oldschool_spec.rb
@@ -1,7 +1,25 @@
 require 'spec_helper'
 
 describe RsHighscores::OldSchool::Player do
-  it "namespace alias" do
-    OSRS::Player.should eq(RsHighscores::OldSchool::Player)
+  context "backwards compatibility" do
+    it "namespace alias" do
+      OSRS::Player.should eq(RsHighscores::OldSchool::Player)
+    end
+  end
+
+  context "usability" do
+    before :all do
+      @player = OSRS::Player.new "jebrim", :force
+      @stats = OSRS::Stats.new @player.raw_stats
+    end
+
+    it "skill_names output" do
+      OSRS::Stats::Skills.should eq(%w(Overall Attack Defence Strength
+                                      Hitpoints Ranged Prayer Magic
+                                      Cooking Woodcutting Fletching Fishing
+                                      Firemaking Crafting Smithing Mining
+                                      Herblore Agility Thieving Slayer
+                                      Farming Runecrafting Hunter Construction))
+    end
   end
 end
